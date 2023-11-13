fe32d25356b3df4541b5036b624ad6f55ec160c5
oscarm8 <oscar.roman229303@potros.itson.edu.mx>
2023-11-12 20:28:50 -0700

    Arreglados los imports de los componentes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import { Header } from "./HeaderComponent/header.js";
-import { Banner } from "./BannerComponent/banner.js";
-import { Footer } from "./FooterComponent/footer.js";
+import { Header } from "../components/HeaderComponent/header.js";
+import { Banner } from "../components/BannerComponent/banner.js";
+import { Footer } from "../components/FooterComponent/footer.js";
 
 window.customElements.define('header-info', Header);
 window.customElements.define('banner-info', Banner);
