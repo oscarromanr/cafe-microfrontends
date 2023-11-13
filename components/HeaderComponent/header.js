1d2fe263e956f498f21bb4a82b4f22ec7a2e86c7
oscarm8 <oscar.roman229303@potros.itson.edu.mx>
2023-11-12 20:26:02 -0700

    Instalacion de tailwindcss, modificación de la estructura del proyecto, header component terminado, componentes modificados

---
+++ components/HeaderComponent/header.js
@@ -0,0 +1,19 @@
+export class Header extends HTMLElement{
+    constructor(){
+        super();
+    }
+
+    connectedCallback(){
+        const shadow = this.attachShadow({mode: "open"});
+        this.#render(shadow);
+    }
+
+    async #render(shadow){
+        await fetch('../components/HeaderComponent/header.html')
+            .then(response => response.text())
+            .then(html => {
+                shadow.innerHTML += html;
+            })
+            .catch(error => console.error('Error loading HTML: ', error));
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { Header } from "../components/HeaderComponent/header.js";
+import { Banner } from "../components/BannerComponent/banner.js";
+import { Footer } from "../components/FooterComponent/footer.js";
+
+window.customElements.define('header-info', Header);
+window.customElements.define('banner-info', Banner);
+window.customElements.define('footer-info', Footer);
