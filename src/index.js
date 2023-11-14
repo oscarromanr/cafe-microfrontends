02fb360d8400687f54636501ffaaf7d765e8a576
MiguelAngelDeLaRosa <miguel.delarosa229290@potros.itson.edu.mx>
2023-11-13 19:52:32 -0600

    FeaturedComponent
    Falta alinear bien las imagenes y agrandar mas el texto

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import { Header } from "../components/HeaderComponent/header.js";
 import { Banner } from "../components/BannerComponent/banner.js";
 import { Footer } from "../components/FooterComponent/footer.js";
 import { Content } from "../components/ContentComponent/content.js";
+import { FeaturedProduct } from "../components/FeaturedComponent/featuredProduct.js";
 
 window.customElements.define('header-info', Header);
 window.customElements.define('banner-info', Banner);
 window.customElements.define('footer-info', Footer);
 window.customElements.define('content-info', Content);
+window.customElements.define('featured-info', FeaturedProduct);
