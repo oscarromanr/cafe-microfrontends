1b91a7e8c77ca094473939f17107919456ad8132
oscarm8 <oscar.roman229303@potros.itson.edu.mx>
2023-11-13 22:49:57 -0700

    ContactComponent (falta formulario responsivo), Featured component modificado (no terminado)

---
+++ src/contact.js
@@ -0,0 +1,7 @@
+import { Header } from "../components/HeaderComponent/header.js";
+import { Footer } from "../components/FooterComponent/footer.js";
+import { Contact } from "../components/ContactComponent/contact.js";
+
+window.customElements.define('header-info', Header);
+window.customElements.define('footer-info', Footer);
+window.customElements.define('contact-info', Contact);
